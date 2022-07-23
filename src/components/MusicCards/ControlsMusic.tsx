ad15132621c5f98cdcd4038900c81719c78889bf
_ignacio.dev_ <protec83007@gmail.com>
2022-07-23 14:13:56 -0300

    Controls (Bug)

--- src/components/MusicCards/ControlsMusic.tsx
+++ src/components/MusicCards/ControlsMusic.tsx
@@ -1,43 +1,93 @@
-import { useEffect, useState } from 'react'
-import { Image, Progress, Text } from '@chakra-ui/react';
+import React, { useState, useRef, useEffect} from 'react'
+import {Box, Image, Text} from '@chakra-ui/react'
 import './controlMusic.css'
 
 const ControlsMusic = () => {
-const [isPlaying, setIsPlaying] = useState(false);
+    //state
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [duration, setDuration] = useState(0)
+    const [progress, setProgress] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0)
 
+    //refereces
+    const audioPlayer = useRef() as React.RefObject<HTMLAudioElement>;
+
+    //effects
+    useEffect(() => {
+        if (!audioPlayer.current) return
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds);
+    }, [audioPlayer?.current?.onloadedmetadata, audioPlayer?.current?.readyState]);
+
+    useEffect(() => {
+        if (!audioPlayer.current) return
+        audioPlayer.current.addEventListener('timeupdate', handleProgress);
+        setCurrentTime(audioPlayer.current.currentTime);
+    }
+    , [])
+
+    //functions
+    const togglePlayPause = () => {
+        if (!audioPlayer.current) return
+        const prevValue = isPlaying;
+        setIsPlaying(!prevValue);
+        if (!prevValue) {
+            audioPlayer.current.play();
+        } else {
+            audioPlayer.current.pause();
+        }
+    }
+
+    const handleProgress = () => {
+        if (!audioPlayer.current) return
+        setProgress(audioPlayer.current.currentTime / audioPlayer.current.duration * 100);
+    }
+
+    const calculateTime = (secs: number) => {
+        const minutes = Math.floor(secs / 60);
+        const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+        const seconds = Math.floor(secs % 60);
+        const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        return `${returnedMinutes}:${returnedSeconds}`;
+    }
+ 
   return (
-    //! Controles de Musica
     <div className='controlMusic__container'>
-        <div className='controlMusic__button__container'>
-            <button className='controlMusic__button' onClick={() => setIsPlaying(!isPlaying)}>
-                {isPlaying ? 
-                   <Image
-                    id='pauseMusic' 
-                    className='controlMusic__svg' 
-                    src="src/assets/svg/pause.svg" 
-                    alt='pauseSvg'
-                   /> : <Image
-                    id='playMusic'
-                    className='controlMusic__svg' 
-                    src="src/assets/svg/play.svg" 
-                    alt='playSvg'
-                   /> 
-                }
-            </button>
-        </div>
+        <audio ref={audioPlayer} src='src/assets/music/music-3.mp3'></audio>
+        <button className='controlMusic__button__container' onClick={togglePlayPause}>
+            {isPlaying ? 
+            <Image
+            className='controlMusic__svg' 
+            src="src/assets/svg/pause.svg" 
+            alt='pauseSvg'
+           /> : <Image
+            className='controlMusic__svg' 
+            src="src/assets/svg/play.svg" 
+            alt='playSvg'
+           />
+            }
+        </button>
+
+        {/* Progress Bar */}
         <progress
+            id='progressBar'
             className='progress__bar'
-            value={26}
+            value={progress}
             max={100}
         >
         </progress>
+        
+
+        {/* Current Time */}
         <Text 
             className='controlMusic__time' 
             fontFamily="music_card_font"  
             color="white" 
-            fontSize='xs'
+            fontSize="12px"
         >
-        03:23</Text>
+        {calculateTime(duration)} / {calculateTime(currentTime)}
+        </Text>
+
     </div>
   )
 }
